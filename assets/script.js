e0fe7e938f62aab18d49c33c5d856b2644a0e281
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2022-01-14 10:29:55 -0800

    created handleProjectFormSubmit to find obtain values and pass them to the printProjectData

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,21 @@ var dueDateInputEl = $('#due-date-input');
 
 
 
-
+// handle project form submission
+function handleProjectFormSubmit(event) {
+  event.preventDefault();
+// obtaining the value and triming the white space 
+  var projectName = projectNameInputEl.val().trim();
+  var projectType = projectTypeInputEl.val().trim();
+  var hourlyRate = hourlyRateInputEl.val().trim();
+  var dueDate = dueDateInputEl.val().trim();
+ 
+  //passing parameters on function call which then will be received by the function as arguments
+  printProjectData(projectName, projectType, hourlyRate, dueDate);
+  
+  //reseting the first projectFormEl 
+  projectFormEl[0].reset();
+}
 
 projectFormEl.on('submit', handleProjectFormSubmit);
 projectDisplayEl.on('click', '.delete-project-btn', handleDeleteProject);
